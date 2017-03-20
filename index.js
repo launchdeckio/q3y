743db00197a2889098b8878e1b1ae99106499e30
JM Versteeg <janmerijnversteeg@gmail.com>
2017-03-20 18:57:13 +0100

    expose more submodules

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ module.exports.deep     = require('./lib/higherOrder/deep');
 module.exports.hub      = require('./lib/higherOrder/hub');
 module.exports.onDemand = require('./lib/higherOrder/onDemand');
 module.exports.byKey    = require('./lib/higherOrder/byKey');
+
+module.exports.NoDataAtPathError = require('./lib/errors/NoDataAtPathError');
+
+const constants = require('./lib/constants');
+
+module.exports.erase       = constants.erase;
+module.exports.transparent = constants.transparent;
